6d4a95a10e5f17557c51c1d9cae1ef09a3af924e
Lautaro Diez <ldiez@q4tech.com>
2022-08-05 17:16:52 -0300

    Create pages

---
+++ SurveysApp/SurveysApp/Views/RegistrationPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using SurveysApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SurveysApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegistrationPage : ContentPage
+    {
+        private RegisterViewModel _viewModel;
+
+        public RegistrationPage()
+        {
+            InitializeComponent();
+            BindingContext = _viewModel = new RegisterViewModel();
+        }
+    }
+}
